8c91f9bec6c49874b7c94e8dc58abac402ab0db4
Qiang Liu <cyruscyliu@gmail.com>
2022-03-14 23:08:42 +0100

    enable videzzo less crashes

--- videzzo_qemu/0005-compile-qemu-cov.sh
+++ videzzo_qemu/0005-compile-qemu-cov.sh
@@ -7,7 +7,7 @@ CLANG_COV_DUMP=1 \
 CC=clang CXX=clang++ ../configure \
     --enable-videzzo --enable-fuzzing --enable-debug \
     --disable-werror --disable-sanitizers \
-    --extra-cflags="-DCLANG_COV_DUMP -DVIRTFUZZ_LESS_CRASHES -fprofile-instr-generate -fcoverage-mapping" \
+    --extra-cflags="-DCLANG_COV_DUMP -DVIDEZZO_LESS_CRASHES -fprofile-instr-generate -fcoverage-mapping" \
     --target-list="i386-softmmu arm-softmmu"
 ninja qemu-fuzz-i386 qemu-system-i386 qemu-fuzz-arm qemu-system-arm
 popd
