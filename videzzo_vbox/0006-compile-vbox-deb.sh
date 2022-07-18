31caa74193dae4aab7bdaf0590e13ad2ade4baf2
Qiang Liu <cyruscyliu@gmail.com>
2022-07-18 14:21:20 +0800

    [Support VirtualBox] 32 enable partial ASAN (VBoxDD.so)

--- videzzo_vbox/0006-compile-vbox-deb.sh
+++ videzzo_vbox/0006-compile-vbox-deb.sh
@@ -9,13 +9,12 @@ ANNOTATION="-videzzo-instrumentation=$PWD/videzzo_vbox_types.yaml -flegacy-pass-
 EXPORT_SYMBOL_LIST="$PWD/export_symbol_list.txt"
 EXPORT_SYMBOL="-Wl,--export-dynamic -Wl,--export-dynamic-symbol-list=$EXPORT_SYMBOL_LIST"
 kmk VBOX_FUZZ=1 KBUILD_TYPE=debug VBOX_GCC_TOOL=CLANG \
-    VBOX_WITH_GCC_SANITIZER=1 \
-    IPRT_WITH_GCC_SANITIZER=1 \
     PATH_OUT_BASE=$PWD/out-deb \
-    TOOL_CLANG_CFLAGS="-fsanitize=fuzzer-no-link -fPIE ${ANNOTATION}" \
-    TOOL_CLANG_CXXFLAGS="-fsanitize=fuzzer-no-link -fPIE ${ANNOTATION}" \
+    TOOL_CLANG_CFLAGS="-fsanitize=fuzzer-no-link -fPIE -DRT_NO_STRICT ${ANNOTATION}" \
+    TOOL_CLANG_CXXFLAGS="-fsanitize=fuzzer-no-link -fPIE -DRT_NO_STRICT ${ANNOTATION}" \
     TOOL_CLANG_LDFLAGS="-fsanitize=fuzzer-no-link ${EXPORT_SYMBOL}" \
-    VBOX_FUZZ_LDFLAGS="-fsanitize=fuzzer"
+    VBOXDD_SANITIZER=1 \
+    VBOX_FUZZ_LDFLAGS="-fsanitize=fuzzer,address,undefined"
 
 # 1. compile kernel drivers
 pushd out-deb/linux.amd64/debug/bin/
